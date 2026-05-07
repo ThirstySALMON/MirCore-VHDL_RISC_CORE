77af24abfb3f21fbe666feceb3a39d6d4c2090c4
Salma Marei-github <salmamarei2@gmail.com-github>
2026-05-07 15:24:11 +0300

    fwd unit base structure

---
+++ src/01_components/forwarding_unit.vhd
@@ -0,0 +1,105 @@
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+
+entity forwarding_unit is
+  port (
+    -- inputs from id/ex1
+    rsrc1_addr           : in  std_logic_vector(2 downto 0);
+    rsrc2_addr           : in  std_logic_vector(2 downto 0);
+
+    -- inputs from ex1/ex2
+    swap_state           : in  std_logic_vector(1 downto 0);
+    reg_WE_ex1_ex2       : in  std_logic;
+    reg_dst_addr_ex1_ex2 : in  std_logic_vector(2 downto 0);
+    alu_op_ex1_ex2       : in  std_logic_vector(2 downto 0);
+    reg_data_ex1_ex2     : in  std_logic_vector(2 downto 0);
+
+    -- inputs from ex2/mem
+    reg_data_ex2_mem     : in  std_logic_vector(2 downto 0);
+    reg_dst_addr_ex2_mem : in  std_logic_vector(2 downto 0);
+    alu_op_ex2_mem       : in  std_logic_vector(2 downto 0);
+    reg_WE_ex2_mem       : in  std_logic;
+
+    -- inputs from mem/wb
+    is_load              : in  std_logic;
+    reg_WE_mem_wb        : in  std_logic;
+    reg_dst_addr_mem_wb  : in  std_logic_vector(2 downto 0);
+    alu_op_mem_wb        : in  std_logic_vector(2 downto 0);
+    reg_data_mem_wb      : in  std_logic_vector(2 downto 0);
+
+    -- outputs to ex1
+    operand1_sel         : out std_logic_vector(2 downto 0);
+    operand2_sel         : out std_logic_vector(2 downto 0)
+  );
+end entity;
+
+architecture rtl of forwarding_unit is
+
+begin
+
+  process (all)
+  begin
+    -- default is rsrc
+    operand1_sel <= (others => '0');
+    operand2_sel <= (others => '0');
+
+    --OPERAND 1
+    -- check ex1/ex2 first
+    if ((reg_WE_ex1_ex2 = '1') and (reg_dst_addr_ex1_ex2 = rsrc1_addr)) then
+      if (reg_data_ex1_ex2 = "010") then
+        operand1_sel <= "101";
+      elsif (reg_data_ex1_ex2 = "011") then
+        operand1_sel <= "001";
+      end if;
+
+      --check ex2/mem
+    elsif ((reg_WE_ex2_mem = '1') and (reg_dst_addr_ex2_mem = rsrc1_addr)) then
+      if (reg_data_ex2_mem = "010") then
+        operand1_sel <= "110";
+      elsif (reg_data_ex2_mem = "011") then
+        operand1_sel <= "010";
+      end if;
+
+      --check mem/wb
+    elsif ((reg_WE_mem_wb = '1') and (reg_dst_addr_mem_wb = rsrc1_addr)) then
+      if (reg_data_mem_wb = "001") then
+        operand1_sel <= "100";
+      elsif (reg_data_mem_wb = "010") then
+        operand1_sel <= "111";
+      elsif (reg_data_mem_wb = "011") then
+        operand1_sel <= "011";
+      end if;
+
+    end if;
+
+        --OPERAND 2
+    -- check ex1/ex2 first
+    if ((reg_WE_ex1_ex2 = '1') and (reg_dst_addr_ex1_ex2 = rsrc2_addr)) then
+      if (reg_data_ex1_ex2 = "010") then
+        operand2_sel <= "101";
+      elsif (reg_data_ex1_ex2 = "011") then
+        operand2_sel <= "001";
+      end if;
+
+      --check ex2/mem
+    elsif ((reg_WE_ex2_mem = '1') and (reg_dst_addr_ex2_mem = rsrc2_addr)) then
+      if (reg_data_ex2_mem = "010") then
+        operand2_sel <= "110";
+      elsif (reg_data_ex2_mem = "011") then
+        operand2_sel <= "010";
+      end if;
+
+      --check mem/wb
+    elsif ((reg_WE_mem_wb = '1') and (reg_dst_addr_mem_wb = rsrc2_addr)) then
+      if (reg_data_mem_wb = "001") then
+        operand2_sel <= "100";
+      elsif (reg_data_mem_wb = "010") then
+        operand2_sel <= "111";
+      elsif (reg_data_mem_wb = "011") then 
+        operand2_sel <= "011";
+      end if;
+
+    end if;
+  end process;
+end architecture;
